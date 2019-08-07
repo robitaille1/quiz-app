b9b8dca53e3761ac9cca0e68af56f86a77b05d72
Lucas Robitaille <lrjames237@gmail.com>
2019-08-07 08:35:28 -0500

    Added index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+//Home Screen
+
+function startQuiz {
+    //When start button clicked
+
+    //render first question
+}
+
+//Questions Pages
+function submitAnswer{
+    //When answer selected and button submitted
+
+    //render correct or incorrect page
+}
+
+function answerIsCorrect {
+    //if selected answer is correct
+        //render correct page
+    //if selected answer is incorrect
+        //render incorrect page 
+}
+
+function numberCounter {
+    //increment by 1 at each question
+}
+
+function correctAnswers {
+    //increment by one for each correct answer
+}
+
+function nextQuestion {
+    //When button clicked render the next question
+}
+
+//Overall Page
+
+function overallPage {
+    //After last question render the overall score page
+    //When button clicked render main screen again
+}
