2642a48eb223e7a8ac8e0207522215dfa816254d
Lucas Robitaille <lrjames237@gmail.com>
2019-08-07 14:49:55 -0400

    begin work on js logic

--- index.js
+++ index.js
@@ -1,40 +1,123 @@
+let questionNum = 0;
+let score =0;
+
 //Home Screen
-
-function startQuiz {
+function startQuiz() {
     //When start button clicked
-
-    //render first question
+   $('.start').on('click', function(){
+    event.preventDefault();
+    console.log('Start quiz button was clicked');
+    $('.js-hide-it').remove();
+    renderQuestions();
+   });
 }
 
 //Questions Pages
-function submitAnswer{
+function renderQuestions() {
+    
+    if(questionNum < STORE.length) {
+        $(numberCounter);
+        $('.display').html(`
+            <section role='Quiz Questions' class="container question">
+                <h2>${STORE[questionNum].question}</h2>
+                <form>
+                    <button class="answer">${STORE[questionNum].option1}</button>
+                    <button class="answer">${STORE[questionNum].option2}</button>
+                    <button class="answer">${STORE[questionNum].option3}</button>
+                    <button class="answer">${STORE[questionNum].option4}</button>
+                    <button type='button' class='submit'>SUBMIT</button>
+                </form>
+            </section>`
+        );
+    } else {
+        overallPage();
+    }
+}
+
+function submitAnswer() {
     //When answer selected and button submitted
-
-    //render correct or incorrect page
+    $('body').on('click', '.submit', function(){
+        event.preventDefault();
+        console.log('Submit button clicked');
+        $('.question').remove();
+        correctAnswerPage();
+    });
 }
 
-function answerIsCorrect {
+function correctAnswerPage() {
     //if selected answer is correct
-        //render correct page
+    $('.display').append(`
+    <section role='Correct Answer' class="container js-correct-answer">
+        <h2 class="Correct">Correct Answer!!</h2>
+        <form>
+            <button type='button' class='correct-btn'>Next Question</button>
+        </form>
+    </section>`
+    );
+    correctAnswers();
+}
+
+function incorrectAnswerPage(){
     //if selected answer is incorrect
-        //render incorrect page 
+    $('.display').append(`
+        <section role='Incorrect Answer' class="container">
+            <h2 class="incorrect">Incorrect Answer</h2>
+            <p>The correct answer is: <span class="incorrect">${STORE[questionNum].correctAnswer}</span></p>
+            <form>
+                <button class='incorrect-btn'>Next Question</button>
+            </form>
+        </section>`
+    );
 }
 
-function numberCounter {
+//Answer page button - move to next Question
+function nextQuestion(){
+    $('body').on('click', '.correct-btn', function(){
+        console.log('next question clicked');
+        event.preventDefault();
+        $('.js-correct-answer').remove();
+        $(renderQuestions);
+    });
+}
+
+function numberCounter() {
     //increment by 1 at each question
+    questionNum ++;
+    $('.currentQ').text(questionNum);
 }
 
-function correctAnswers {
+function correctAnswers() {
     //increment by one for each correct answer
-}
-
-function nextQuestion {
-    //When button clicked render the next question
+    score++;
+    $('.currentScore').text(score);
 }
 
 //Overall Page
 
-function overallPage {
+function overallPage() {
     //After last question render the overall score page
+    $('body').append(`
+    <section role='Overall Results' class="container js-overall">
+        <h2>Congratulations!</h2>
+        <p>You got <span class="amtCorrect">8</span> out of 10 correct!!</p>
+        <form>
+            <button class='restart'>Restart the Quiz</button>
+        </form>
+    </section>`
+    );
+    $('.amtCorrect').text(score);
+
     //When button clicked render main screen again
 }
+
+function restartQuiz(){
+    $('body').on('click', '.restart', function(){
+        $('.js-overall').remove();
+        $('.js-hide-it').display();
+    });
+}
+
+
+$(startQuiz);
+$(submitAnswer);
+$(nextQuestion);
---
+++ questions.js
@@ -0,0 +1,93 @@
+const STORE = [
+    {
+        questionNum: 1,
+        question: 'Which neighborhood in NYC does Jerry live?',
+        option1: 'Upper East Side',
+        option2: "Hell's Kitchen",
+        option3: 'Upper West Side',
+        option4: 'Midtown',
+        correctAnswer: 'Upper East Side'
+    },
+    {
+        questionNum: 2,
+        question: 'What type of salad does Elaine order when George is paying for lunch? ',
+        option1: 'Caesar Salad',
+        option2: 'Greek Salad',
+        option3: 'Big Salad',
+        option4: 'Fruit Salad',
+        correctAnswer: 'Big Salad'
+    },
+    {
+        questionNum: 3,
+        question: ' Which real life actress does George fall in love with?',
+        option1: 'Terri Hatcher',
+        option2: 'Marisa Tomei',
+        option3: 'Courtney Cox',
+        option4: 'Uma Thurman',
+        correctAnswer: 'Marisa Tomei'
+    },
+    {
+        questionNum: 4,
+        question: 'Where is Elaine originally from? ',
+        option1: 'Maryland',
+        option2: 'New York',
+        option3: 'Maine',
+        option4: 'Ohio',
+        correctAnswer: 'Maryland'
+    },
+    {
+        questionNum: 5,
+        question: 'Which religion does George attempt to convert to? ',
+        option1: 'Judaism',
+        option2: 'Christianity',
+        option3: 'Russian Orthodox',
+        option4: 'Latvian Orthodox',
+        correctAnswer: 'Latvian Orthodox'
+    },
+    {
+        questionNum: 6,
+        question: "What is Kramer's name in the pilot episode?",
+        option1: 'Kramer',
+        option2: 'Kosmo',
+        option3: 'Kessler',
+        option4: 'Kensington',
+        correctAnswer: 'Kessler'
+    },
+    {
+        questionNum: 7,
+        question: "Who is Jerry's arch nemesis? ",
+        option1: 'Banya',
+        option2: 'Newman',
+        option3: 'Soup Nazi',
+        option4: 'Mike Moffit',
+        correctAnswer: 'Newman'
+    },
+    {
+        questionNum: 8,
+        question: "What are George's parents names?",
+        option1: 'Fred and Ethel',
+        option2: 'Fred and Estelle',
+        option3: 'Frank and Ethel',
+        option4: 'Frank and Estelle',
+        correctAnswer: 'Frank and Estelle'
+    },
+    {
+        questionNum: 9,
+        question: 'When Kramer gets the wrong license plate, what does it say? ',
+        option1: 'Fusilli',
+        option2: 'Assman',
+        option3: 'Kramer',
+        option4: 'I luv NY',
+        correctAnswer: 'Assman'
+    },
+    {
+        questionNum: 10,
+        question: "Who is the first to drop out of 'The Contest'?",
+        option1: 'Jerry',
+        option2: 'George',
+        option3: 'Kramer',
+        option4: 'Elaine',
+        correctAnswer: 'Kramer'
+    }
+    
+]
